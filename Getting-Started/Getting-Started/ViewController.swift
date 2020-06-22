500dbec7717810a46fc94eeafa3d800eb37af9ad
Kilo Loco <kiloloco@me.com>
2020-06-21 17:52:38 -0700

    amplify init

---
+++ Getting-Started/Getting-Started/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Getting-Started
+//
+//  Created by Kyle Lee on 6/21/20.
+//  Copyright © 2020 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
